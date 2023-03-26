9f7279718532d6cff1a076a1819c185e56a89c93
followspire <128288919+followspire@users.noreply.github.com>
2023-03-26 15:39:30 +0300

    minor

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import tw from 'twin.macro';
 import Landing from './views/Landing';
 // import Spire from './views/Spire';
 
-const Container = tw.div`h-screen w-screen bg-gradient-to-b from-gray-400 to-gray-600 flex flex-col justify-center items-center text-center overflow-hidden`;
+const Container = tw.div`h-screen w-screen bg-gradient-to-b from-gray-400 to-gray-600 flex flex-col justify-center items-center text-center overflow-hidden px-1`;
 
 function App() {
   return (
